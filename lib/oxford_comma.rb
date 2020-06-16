3187586fced9d57ba869900efa725cbe743dfb8a
devWRM <mcwen@comcast.net>
2020-06-16 18:10:18 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,26 @@
 require 'pry'
 
 def oxford_comma(array)
-
-    if array.length === 1
-        array.join
-    elsif array.length === 2
-        array.join(" and ")
-    else 
-        # array.length === 3
-        x = array.pop
-        x = "and " + x
-        array << x
-        array.join(", ")
+    if array.length == 2
+      return "#{array[0]} and #{array[1]}"
+    elsif 2 < array.length
+      array[-1].insert(0, "and ")
     end
-end
+    array.join(", ")    
+  end
+
+
+# def oxford_comma(array)
+
+#     if array.length === 1
+#         array.join
+#     elsif array.length === 2
+#         array.join(" and ")
+#     else 
+#         # array.length === 3
+#         x = array.pop
+#         x = "and " + x
+#         array << x
+#         array.join(", ")
+#     end
+# end
