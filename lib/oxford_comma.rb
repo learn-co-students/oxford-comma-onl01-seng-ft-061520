5f74c79c0a98597e5a77cb9d1e57c4a0ffb6e3e0
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-06-17 14:25:04 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
-def oxford_comma(array)
+# frozen_string_literal: false
 
+def oxford_comma(array)
+  if array.size < 3
+    array.join(' and ')
+  else
+    array[-1] = 'and ' + array[-1]
+    array.join(', ')
+  end
 end
