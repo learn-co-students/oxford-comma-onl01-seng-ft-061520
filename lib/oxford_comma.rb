1e4832c659cfa6f55eee810751e10eec2889d0ca
Jose Hidago <jhidalgo521@gmail.com>
2020-06-20 20:24:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  case array.length
+  when 0
+    array.first
+  when 1
+    array.join('and')
+  else
+    [array[0..-2].join(', '), array.last].join(', and ')
+  end
 end
